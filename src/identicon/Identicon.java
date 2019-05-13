3379028fcbf0c22d86465fb3610f83d4c6402cb4
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-05-13 11:26:22 +0200

    Implement Object-oriented Approach

---
+++ src/identicon/Identicon.java
@@ -0,0 +1,184 @@
+package identicon;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Identicon {
+
+    private Color foreground;
+    private int[] foregroundRGBA;
+
+    private Color background;
+    private int[] backgroundRGBA;
+
+    private String text;
+    private byte[] hashedText;
+    private String hashingAlgorithm;
+
+    private BufferedImage identicon;
+    private WritableRaster raster;
+    private int borderPixels;
+    private int identiconPixels;
+    private int totalPixels;
+
+    private Image image;
+    private BufferedImage bufferedImage;
+    private int imagePixels;
+
+
+    // Singleton Fields and Methods
+
+    private static Identicon instance = new Identicon();
+
+    public static Identicon getInstance() {
+        return instance;
+    }
+
+    private Identicon() {
+        // Default Values
+        text             = "YoussefRaafatNasry";
+        hashingAlgorithm = "SHA-256";
+        borderPixels     = 1;
+        identiconPixels  = 5;
+        imagePixels      = 250;
+        generateIdenticon();
+        generateImage();
+    }
+
+
+    //// Public Methods ////
+
+    public void generateIdenticon() {
+        hashText();
+        determineForeground();
+        determineBackground();
+        initIdenticon();
+        drawBorder();
+        drawIdenticon();
+    }
+
+    public void generateImage() {
+
+        BufferedImage scaledImage = new BufferedImage(imagePixels, imagePixels, BufferedImage.TYPE_INT_ARGB);
+
+        AffineTransform transform = new AffineTransform();
+        int scale = imagePixels / totalPixels;
+        transform.scale(scale, scale);
+
+        AffineTransformOp transformOp = new AffineTransformOp(transform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+        scaledImage = transformOp.filter(identicon, scaledImage);
+
+        image = SwingFXUtils.toFXImage(scaledImage, null);
+
+    }
+
+
+    //// Private Methods ////
+
+    private void hashText() {
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance(hashingAlgorithm);
+        } catch(NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+
+        }
+        hashedText = messageDigest.digest(text.getBytes());
+    }
+
+    private void determineForeground() {
+        int r = foreground == null ? hashedText[0] & 255 : (int) (foreground.getRed() * 255);
+        int g = foreground == null ? hashedText[1] & 255 : (int) (foreground.getGreen() * 255);
+        int b = foreground == null ? hashedText[2] & 255 : (int) (foreground.getBlue() * 255);
+        int a = foreground == null ? 255 : (int) (foreground.getOpacity() * 255);
+        foregroundRGBA = new int[]{r, g, b, a};
+    }
+
+    private void determineBackground() {
+        int r = background == null ? 255 : (int) (background.getRed() * 255);
+        int g = background == null ? 255 : (int) (background.getGreen() * 255);
+        int b = background == null ? 255 : (int) (background.getBlue() * 255);
+        int a = background == null ? 255 : (int) (background.getOpacity() * 255);
+        backgroundRGBA = new int[]{r, g, b, a};
+    }
+
+    private void initIdenticon() {
+        totalPixels = identiconPixels + 2 * borderPixels;
+        identicon = new BufferedImage(totalPixels, totalPixels, BufferedImage.TYPE_INT_ARGB);
+        raster = identicon.getRaster();
+    }
+
+    private void drawBorder() {
+        for (int x = 0; x < totalPixels; x++) {
+            for (int y = 0; y < totalPixels; y++) {
+                boolean isBorderX = x < borderPixels || x >= totalPixels - borderPixels;
+                boolean isBorderY = y < borderPixels || y >= totalPixels - borderPixels;
+                if (isBorderX || isBorderY) {
+                    raster.setPixel(x, y, backgroundRGBA);
+                }
+            }
+        }
+    }
+
+    private void drawIdenticon() {
+        for (int x = 0; x < identiconPixels; x++) {
+            int i = x < 3 ? x : identiconPixels - 1 - x;
+            for (int y = 0; y < identiconPixels; y++) {
+                if ((hashedText[i] >> y & 1) == 1) {
+                    raster.setPixel(x + borderPixels, y + borderPixels, foregroundRGBA);
+                } else {
+                    raster.setPixel(x + borderPixels, y + borderPixels, backgroundRGBA);
+                }
+            }
+        }
+    }
+
+
+    //// Getter and Setters ////
+
+    public Image getImage() {
+        return image;
+    }
+
+    public BufferedImage getBufferedImage() {
+        return SwingFXUtils.fromFXImage(image, null);
+    }
+
+    public void setImagePixels(int imagePixels) {
+        this.imagePixels = imagePixels;
+    }
+
+    public void setForeground(Color foreground) {
+        this.foreground = foreground;
+    }
+
+    public void setBackground(Color background) {
+        this.background = background;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setHashingAlgorithm(String hashingAlgorithm) {
+        this.hashingAlgorithm = hashingAlgorithm;
+    }
+
+    public void setBorderPixels(int borderPixels) {
+        this.borderPixels = borderPixels;
+    }
+
+    public void setIdenticonPixels(int identiconPixels) {
+        this.identiconPixels = identiconPixels;
+    }
+
+
+}
--- src/identicon/IdenticonGeneratorController.java
+++ src/identicon/IdenticonGeneratorController.java
@@ -3,33 +3,21 @@ package identicon;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class IdenticonGeneratorController {
 
-    private final int BORDER_SIZE   = 1;                               // Border Size: 1px
-    private final int ORIGINAL_SIZE = 5;                               // Identicon Size: 5px x 5px
-    private final int TOTAL_SIZE    = ORIGINAL_SIZE + 2 * BORDER_SIZE; // Generated Identicon Size: 7px x 7px
-    private final int PREVIEW_SIZE  = 250;                             // PREVIEW Size: 250px x 250
-
-    private BufferedImage identicon;
+    private Identicon identicon;
+    private final int PREVIEW_SIZE = 250;
 
     @FXML public TextField inputTextField;
     @FXML public ComboBox<String> hashingAlgorithmsComboBox;
@@ -44,11 +32,16 @@ public class IdenticonGeneratorController {
     @FXML
     public void initialize() {
 
-        // Set Preview Dimensions
+        identicon = Identicon.getInstance();
+
+        // Set Preview Dimensions & PlaceHolder Image
+
         identiconImageView.setFitHeight(PREVIEW_SIZE);
         identiconImageView.setFitWidth(PREVIEW_SIZE);
+        identiconImageView.setImage(identicon.getImage());
 
         // Initial Values
+
         hashingAlgorithmsComboBox.setItems(FXCollections.observableArrayList("MD5", "SHA-1", "SHA-256"));
         hashingAlgorithmsComboBox.getSelectionModel().select(0);
         foregroundColorPicker.setValue(null);
@@ -90,99 +83,32 @@ public class IdenticonGeneratorController {
         fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
         File file = fileChooser.showSaveDialog(null);
 
-        if (identicon != null && file != null) {
-            String _quality = qualityTextField.getText();
-            int quality = _quality.equals("") ? PREVIEW_SIZE : Integer.parseInt(_quality);
-            Image image = scaleImage(identicon, quality);
-            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+        if (file != null) {
+            String quality = qualityTextField.getText();
+            identicon.setImagePixels(quality.equals("") ? PREVIEW_SIZE : Integer.parseInt(quality));
+            identicon.generateImage();
+            ImageIO.write(identicon.getBufferedImage(), "png", file);
         }
+
     }
+
 
     private void updatePreview() {
-        byte[] hashedText = encrypt(inputTextField.getText(), hashingAlgorithmsComboBox.getValue());
-        Color foreground = foregroundColorPicker.getValue();
-        Color background = backgroundColorPicker.getValue();
-        identicon = generateIdenticon(hashedText, foreground, background);
-        identiconImageView.setImage(scaleImage(identicon, PREVIEW_SIZE));
-    }
 
-    private byte[] encrypt(String textToEncrypt, String hashingAlgorithm) {
+        identicon.setText(inputTextField.getText());
+        identicon.setHashingAlgorithm(hashingAlgorithmsComboBox.getValue());
+        identicon.setForeground(foregroundColorPicker.getValue());
+        identicon.setBackground(backgroundColorPicker.getValue());
+        identicon.setIdenticonPixels(5);
+        identicon.setBorderPixels(1);
+        identicon.generateIdenticon();
 
-        MessageDigest messageDigest = null;
-
-        try {
-            messageDigest = MessageDigest.getInstance(hashingAlgorithm);
-        } catch(NoSuchAlgorithmException e) {
-            System.out.println(e.getMessage());
-
-        }
-
-        return messageDigest.digest(textToEncrypt.getBytes());
+        identicon.setImagePixels(PREVIEW_SIZE);
+        identicon.generateImage();
+        identiconImageView.setImage(identicon.getImage());
 
     }
 
-    private BufferedImage generateIdenticon(byte[] hashedText, Color foregroundColor, Color backgroundColor) {
-
-        // Determine Colors
-
-        int r, g, b, a;
-
-        r = foregroundColor == null ? hashedText[0] & 255 : (int) (foregroundColor.getRed() * 255);
-        g = foregroundColor == null ? hashedText[1] & 255 : (int) (foregroundColor.getGreen() * 255);
-        b = foregroundColor == null ? hashedText[2] & 255 : (int) (foregroundColor.getBlue() * 255);
-        a = foregroundColor == null ? 255 : (int) (foregroundColor.getOpacity() * 255);
-        int[] foreground = new int[]{r, g, b, a};
-
-        r = backgroundColor == null ? 255 : (int) (backgroundColor.getRed() * 255);
-        g = backgroundColor == null ? 255 : (int) (backgroundColor.getGreen() * 255);
-        b = backgroundColor == null ? 255 : (int) (backgroundColor.getBlue() * 255);
-        a = backgroundColor == null ? 255 : (int) (backgroundColor.getOpacity() * 255);
-        int[] background = new int[]{r, g, b, a};
-
-
-        BufferedImage _identicon = new BufferedImage(TOTAL_SIZE, TOTAL_SIZE, BufferedImage.TYPE_INT_ARGB);
-        WritableRaster raster = _identicon.getRaster();
-
-        // Draw Border
-        for (int x = 0; x < TOTAL_SIZE; x++) {
-            for (int y = 0; y < TOTAL_SIZE; y++) {
-                if (x < BORDER_SIZE || x >= TOTAL_SIZE - BORDER_SIZE ||
-                    y < BORDER_SIZE || y >= TOTAL_SIZE - BORDER_SIZE) {
-                    raster.setPixel(x, y, background);
-                }
-            }
-        }
-
-        // Draw Identicon
-        for (int x = 0; x < ORIGINAL_SIZE; x++) {
-            int i = x < 3 ? x : ORIGINAL_SIZE - 1 - x;
-            for (int y = 0; y < ORIGINAL_SIZE; y++) {
-                if ((hashedText[i] >> y & 1) == 1) {
-                    raster.setPixel(x + BORDER_SIZE, y + BORDER_SIZE, foreground);
-                } else {
-                    raster.setPixel(x + BORDER_SIZE, y + BORDER_SIZE, background);
-                }
-            }
-        }
-
-        return _identicon;
-
-    }
-
-    private Image scaleImage(BufferedImage image, int scaleSize) {
-
-        BufferedImage scaledImage = new BufferedImage(scaleSize, scaleSize, BufferedImage.TYPE_INT_ARGB);
-
-        AffineTransform transform = new AffineTransform();
-        int scaleX = scaleSize / image.getWidth();
-        int scaleY = scaleSize / image.getHeight();
-        transform.scale(scaleX, scaleY);
-
-        AffineTransformOp transformOp = new AffineTransformOp(transform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-        scaledImage = transformOp.filter(image, scaledImage);
-
-        return SwingFXUtils.toFXImage(scaledImage, null);
-    }
 
     private class CheckBoxChangeListener implements ChangeListener<Boolean> {
 
